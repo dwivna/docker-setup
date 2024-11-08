ce937b44e8dd4f5edb79c9a0e6c2ffd153de202c
Abhinav Dwivedi <a.dwivedispu@gmail.com>
2024-11-08 23:35:22 +0530

    Redis Test

---
+++ redis/example/src/test/java/com/dwivna/docker/RedisExampleTest.java
@@ -0,0 +1,34 @@
+package com.dwivna.docker;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class RedisExampleTest
+        extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public RedisExampleTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(RedisExampleTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+}
